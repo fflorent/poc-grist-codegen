daf6863288d747e2adaeda05f74ba35e2663cbe2
Florent <florent.git@zeteo.me>
2023-08-25 16:02:23 +0200

    Making work with ReplaceTableData

--- synchronize.ts
+++ synchronize.ts
@@ -1,14 +1,15 @@
 import {
   ColumnsList,
   Grist,
-  RecordsWithoutFields,
-  RecordsWithoutId,
   CreateFields,
   UpdateColumns,
-  GetFields,
+  Fields,
+  RecordsList,
 } from "./generated/index";
 import { program } from "commander";
 import assert from "node:assert";
+import fetch from "node-fetch";
+
 program
   .requiredOption("-b, --bearer <bearer>", "the API key")
   .requiredOption("-s, --source <docId>", "the source document", "aW2PUNfpyxML")
@@ -22,28 +23,21 @@ program.parse();
 
 const opts = program.opts();
 
+const baseApiUrl = "http://localhost:8484/api";
+
 const myGrist = new Grist({
-  BASE: "http://localhost:8484/api",
+  BASE: baseApiUrl,
   TOKEN: opts.bearer,
 });
-const idMap = new Map<number, number>();
 const SOURCE_DOC = opts.source;
 const DEST_DOC = opts.destination;
-
-const columnListToMap = (list: ColumnsList) => {
-  return new Map(list.columns?.map(({ id, fields }) => [id, fields]));
-};
 
 async function getColumnByRef(docId: string, tableId: string, ref: number) {
   const { columns } = await myGrist.columns.listColumns({
     docId,
     tableId,
+    hidden: true,
   });
-  console.log(
-    "search = %i ; refs = %o",
-    ref,
-    columns?.map((col) => col.fields?.colRef)
-  );
   return columns?.find((col) => col.fields?.colRef === ref);
 }
 
@@ -51,11 +45,65 @@ async function getColumnById(docId: string, tableId: string, id: string) {
   const { columns } = await myGrist.columns.listColumns({
     docId,
     tableId,
+    hidden: true,
   });
   return columns?.find((col) => col.id === id);
 }
 
-async function synchronize(tableName: string, requireField: string) {
+class SourceIdLookup {
+  private constructor(private map: Map<string, Map<number, number>>) {}
+
+  get(table: string, sourceId: number) {
+    return this.map.get(table)!.get(sourceId)!;
+  }
+
+  dump(record: RecordsList["records"][0]) {
+    const res = Object.fromEntries(
+      [...this.map.entries()].map(([colId, map]) => {
+        return [colId, map.get(record.fields[colId])];
+      })
+    );
+    // console.log("res = ", res);
+    return res;
+  }
+
+  static async build(columns: ColumnsList, sourceDoc: string, destDoc: string) {
+    const map = new Map<string, Map<number, number>>();
+    const referencesColumns = columns.columns!.filter(
+      (col) => col.fields?.type?.startsWith("Ref:") && !col.fields?.isFormula
+    );
+    const referencedTablesByColId = new Map(
+      referencesColumns.map((col) => [
+        col.id,
+        col.fields!.type!.replace("Ref:", ""),
+      ])
+    );
+    const uniqueReferencedTable = [
+      ...new Set(referencedTablesByColId.values()),
+    ];
+    for (const table of uniqueReferencedTable) {
+      const { records } = await myGrist.records.listRecords({
+        docId: destDoc,
+        tableId: table,
+        hidden: true,
+      });
+      const idBySourceId = new Map(
+        records.map((rec) => [rec.fields.gristHelper_sourceId, rec.id])
+      );
+      map.set(table, idBySourceId);
+    }
+    return new SourceIdLookup(
+      new Map(
+        referencesColumns.map((col) => [
+          col.id!,
+          map.get(referencedTablesByColId.get(col.id)!)!,
+        ])
+      )
+    );
+  }
+}
+
+async function synchronize(tableName: string) {
   assert.notEqual(
     SOURCE_DOC,
     DEST_DOC,
@@ -65,54 +113,47 @@ async function synchronize(tableName: string, requireField: string) {
   const sourceColumns: ColumnsList = await myGrist.columns.listColumns({
     docId: SOURCE_DOC,
     tableId: tableName,
+    hidden: true,
   });
-
-  console.log("sourceColumns = %o", sourceColumns);
-
-  const destColumns: ColumnsList = await myGrist.columns.listColumns({
-    docId: DEST_DOC,
-    tableId: tableName,
-  });
-
-  // const sourceColumnsMap = columnListToMap(sourceColumns);
-  // const destColumnsMap = columnListToMap(destColumns);
-
-  // const destColumnsToDelete = destColumns
-  //   .columns!.filter((col) => !sourceColumnsMap.has(col.id))
-  //   .map((col) => col.id);
-
-  // const destColumnsToAdd = sourceColumns.columns!.filter(
-  //   (col) => !destColumnsMap.has(col.id)
-  // );
 
   const columnsToSend: UpdateColumns = {
-    columns: await Promise.all(
-      sourceColumns.columns!.map(async ({ id, fields }) => {
-        const { ...createFields } = fields as { [index: string]: any };
-        for (const prop of ["parentId", "colRef"]) {
-          delete createFields![prop];
-        }
-        if (createFields.type?.startsWith("Ref")) {
-          const table = createFields.type?.replace("Ref:", "");
-          console.log("table = ", table);
-          console.log("createFields.visibleCol = ", createFields.visibleCol);
-          const visibleColId = (await getColumnByRef(
-            SOURCE_DOC,
-            table,
-            createFields.visibleCol
-          ))!.id!;
-          createFields.visibleCol = (
-            await getColumnById(DEST_DOC, table, visibleColId)
-          )?.fields?.colRef;
-          console.log("createFields.displayCol = ", createFields.displayCol);
-          delete createFields.displayCol;
-        }
-        return {
-          id: id!,
-          fields: createFields as CreateFields,
-        };
-      })
-    ),
+    columns: [
+      ...(await Promise.all(
+        sourceColumns.columns!.map(async ({ id, fields }) => {
+          const { ...createFields } = fields as { [index: string]: any };
+          for (const prop of ["parentId", "colRef"]) {
+            delete createFields![prop];
+          }
+          if (createFields.type?.startsWith("Ref")) {
+            const table = createFields.type?.replace("Ref:", "");
+            const visibleColId = (await getColumnByRef(
+              SOURCE_DOC,
+              table,
+              createFields.visibleCol
+            ))!.id!;
+            createFields.visibleCol = (
+              await getColumnById(DEST_DOC, table, visibleColId)
+            )?.fields?.colRef;
+            const displayColId = (await getColumnByRef(
+              SOURCE_DOC,
+              tableName,
+              createFields.displayCol
+            ))!.id!;
+            createFields.displayCol = (
+              await getColumnById(DEST_DOC, tableName, displayColId)
+            )?.fields?.colRef;
+          }
+          return {
+            id: id!,
+            fields: createFields as CreateFields,
+          };
+        })
+      )),
+      {
+        id: "gristHelper_sourceId",
+        fields: { type: Fields.type.NUMERIC },
+      },
+    ],
   };
 
   await myGrist.columns.replaceColumns({
@@ -121,12 +162,6 @@ async function synchronize(tableName: string, requireField: string) {
     requestBody: columnsToSend,
     replaceall: true,
   });
-
-  const sourceRecs = await myGrist.records.listRecords({
-    tableId: tableName,
-    docId: SOURCE_DOC,
-  });
-  console.log("sourceRecs = ", sourceRecs);
 
   // const recordsToPut: RecordsWithoutId["records"] = sourceRecs.records.map(
   //   (rec) => {
@@ -140,77 +175,110 @@ async function synchronize(tableName: string, requireField: string) {
   // );
   // console.log("recordsToPut = ", recordsToPut);
   //
+
+  await replaceRecords(tableName, sourceColumns);
+
+  // console.log("recordsToPut = ", recordsToPut);
+
+  // await myGrist.records.replaceRecords({
+  //   docId: DEST_DOC,
+  //   tableId: tableName,
+  //   requestBody: {
+  //     records: recordsToPut,
+  //   },
+  // });
+}
+
+async function replaceRecords(tableName: string, sourceColumns: ColumnsList) {
+  const sourceRecs = await myGrist.records.listRecords({
+    tableId: tableName,
+    docId: SOURCE_DOC,
+    hidden: true,
+  });
+
   const formulaColumnIds = sourceColumns
     .columns!.filter((col) => col.fields?.isFormula)
     .map((col) => col.id);
 
+  const sourceIdLookup = await SourceIdLookup.build(
+    sourceColumns,
+    SOURCE_DOC,
+    DEST_DOC
+  );
   const fieldsToOmit = new Set([...formulaColumnIds]);
 
-  const recordsToPut = sourceRecs.records.map((rec) => {
-    const { insee_reg, insee_dep, ...fields } = rec.fields;
-    return {
-      require: { [requireField]: rec.fields[requireField] },
-      id: rec.id,
-      fields: Object.fromEntries(
-        Object.entries(fields).filter(([key]) => !fieldsToOmit.has(key))
+  const destRecords = (
+    await myGrist.records.listRecords({
+      docId: DEST_DOC,
+      tableId: tableName,
+      hidden: true,
+    })
+  ).records!;
+  const destColumns = (
+    await myGrist.columns.listColumns({
+      docId: DEST_DOC,
+      tableId: tableName,
+      hidden: true,
+    })
+  ).columns!;
+  // const destColumnsById = new Map<string, any>(
+  //   destColumns.map((col) => [col.id!, col.fields])
+  // );
+  const destRecordsBySourceId = new Map<number, RecordsList["records"][0]>(
+    destRecords.map((rec) => [rec.fields.gristHelper_sourceId, rec])
+  );
+
+  const body = [
+    [
+      "ReplaceTableData",
+      tableName,
+      sourceRecs.records.map(
+        (rec) => destRecordsBySourceId.get(rec.id)?.id ?? null
       ),
-    };
-  });
-  // console.log("recordsToPut = ", recordsToPut);
-
-  // FIXME: records removed in the source should also be removed in the synchronized table
-  const resReplace: any = await myGrist.records.replaceRecords({
-    docId: DEST_DOC,
-    tableId: tableName,
-    requestBody: {
-      records: recordsToPut,
-    },
-  });
-
-  console.log("resReplace = ", resReplace);
-}
-
-interface SynchronizeParam {
-  tableName: string;
-  requireField: string;
-}
-async function main() {
-  const toSynchronize: Array<SynchronizeParam> = [
-    {
-      tableName: "Regions",
-      requireField: "insee_reg",
-    },
-    {
-      tableName: "Departement",
-      requireField: "insee_dep",
-    },
-    {
-      tableName: "Epci",
-      requireField: "siren_epci",
-    },
-    {
-      tableName: "Communes2",
-      requireField: "insee_com",
-    },
+      {
+        ...Object.fromEntries(
+          sourceColumns.columns!.map(({ id: colId }) => {
+            return [
+              colId,
+              sourceRecs.records!.map((rec) => rec.fields[colId!]),
+            ];
+          })
+        ),
+        gristHelper_sourceId: sourceRecs.records.map((rec) => rec.id),
+        // FIXME sourceIdLookup.dump
+      },
+    ],
   ];
+  console.log("body = ", JSON.stringify(body, null, 4));
+  const res = await fetch(`${baseApiUrl}/docs/${DEST_DOC}/apply`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${opts.bearer}`,
+    },
+    body: JSON.stringify(body), // TODO
+  });
+  console.log("await res.json() = ", await res.json());
+}
+
+async function main() {
+  const tablesToSync = ["Regions" /*, "Departement", "Epci" /*, "Communes2"*/];
   const { tables } = await myGrist.tables.listTables({ docId: DEST_DOC });
-  for (const sync of toSynchronize) {
+  for (const table of tablesToSync) {
     try {
-      if (!tables.some(({ id }) => id === sync.tableName)) {
+      if (!tables.some(({ id }) => id === table)) {
         await myGrist.tables.addTables({
           docId: DEST_DOC,
           requestBody: {
-            tables: [
-              { id: sync.tableName, columns: [{ id: "dummy", fields: {} }] },
-            ],
+            tables: [{ id: table, columns: [{ id: "dummy", fields: {} }] }],
           },
         });
       }
-      await synchronize(sync.tableName, sync.requireField);
+      await synchronize(table);
     } catch (_e) {
       const e = _e as Error;
       console.error(e, e.stack);
-      console.error("Cannot synchronize ", sync.tableName);
+      console.error("Cannot synchronize ", table);
       throw e;
     }
   }
